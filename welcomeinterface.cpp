78827d6a5cbf47577f072cb2f3203b60db3937e6
Eyob Zebene <eyobzebene3580@gmail.com>
2021-11-27 14:03:24 +0300

    the assistant bot

---
+++ welcomeinterface.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int choice;
+    cout << "Hello, I am baymax. Your personal assistant. Are you a new user? \n"
+    << "Then welcome... so, if you are a new user sign up if you are not sign in \n"
+    << "please press '1' to sign in or press '2' to sign up: ";
+    cin >> choice;
+
+    return 0;
+}
+
